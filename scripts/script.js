bdc208f56f5153b28fc4a9b017760b2a9bea8128
yondD <86658209+yondD@users.noreply.github.com>
2021-07-09 15:50:50 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
+let hash = location.hash.substring(1);
+
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?'
 
 headerCityButton.addEventListener('click', () => {
@@ -16,14 +18,14 @@ const disableScroll = () => {
     document.body.dbScrollY = window.scrollY;
 
     document.body.style.cssText = `
-        position: fixed;
-        top: ${-window.scrollY}px;
-        left: 0;
-        width: 100%;
-        hight: 100vh;
-        overflow: hidden;
-        padding-right: ${widthScroll}px;
-    `;
+position: fixed;
+top: ${-window.scrollY}px;
+left: 0;
+width: 100%;
+hight: 100vh;
+overflow: hidden;
+padding-right: ${widthScroll}px;
+`;
 };
 
 const enableScrol = () => {
@@ -48,6 +50,34 @@ const cartModalClose = () => {
     enableScrol();
 };
 
+
+// запрос базы данных
+
+const getData = async() => {
+    const data = await fetch('db.json');
+
+    if (data.ok) {
+        return data.json()
+    } else {
+        throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`)
+    }
+};
+
+const getGoods = (callback, value) => {
+    getData()
+        .then(data => {
+            if (value) {
+                callback(data.filter(item => item.category === value))
+            } else {
+                callback(data);
+            }
+
+        })
+        .catch(err => {
+            console.log(err)
+        });
+};
+
 subHeaderCart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', event => {
@@ -57,3 +87,87 @@ cartOverlay.addEventListener('click', event => {
         cartModalClose();
     }
 });
+
+try {
+
+    const goodsTitle = document.querySelector('.goods__title');
+
+
+
+    const goodsList = document.querySelector('.goods__list');
+
+    if (!goodsList) {
+        throw 'This is not a goods page'
+    }
+
+    const createCard = ({
+            id,
+            preview,
+            cost,
+            brand,
+            name,
+            sizes
+        }) => {
+
+            const li = document.createElement('li');
+
+            li.classList.add('goods__item');
+
+            li.innerHTML = `
+            <article class="good">
+                <a class="good__link-img" href="card-good.html#${id}">
+                    <img class="good__img" src="goods-image/${preview}" alt="">
+                    </a>
+                    <div class="good__description">
+                    <p class="good__price">${cost} &#8381;</p>
+                    <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                    ${sizes ? 
+                        `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` 
+                        : 
+                        ''}
+                    <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                </div>
+            </article>
+                            `;
+
+        return li;
+
+    };
+
+    const renderGoodsLsit = data => {
+    
+        if (hash === 'women'){
+            goodsTitle.textContent = 'Женщинам';
+        } else if( hash === 'men'){
+            goodsTitle.textContent = 'Мужчинам';
+
+        } else if( hash === 'kids'){
+            goodsTitle.textContent = 'Детям';
+        }
+
+        
+
+
+        goodsList.textContent = '';
+
+        data.forEach(item => {
+            const card = createCard(item);
+            goodsList.append(card);
+        });
+
+
+    };
+    
+    window.addEventListener('hashchange', () => {
+
+        
+
+        hash = location.hash.substring(1);    
+        getGoods(renderGoodsLsit, hash);
+    });
+
+    getGoods(renderGoodsLsit, hash);
+
+} catch (err) {
+    console.warn(err)
+}
